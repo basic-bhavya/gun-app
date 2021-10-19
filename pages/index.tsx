cd74e5d6f99e1b89dfa40901e68f513a0f6fe57a
basic-bhavya <bjain.nd@gmail.com>
2021-10-19 15:00:09 +0530

    adding todo stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,48 @@
 import type { NextPage } from "next";
 import Gun from "gun";
+// import "gun/lib/unset";
 import { useEffect, useState } from "react";
 
-const countRef = Gun([
+const gun = Gun([
   "localhost:3000/gun",
   "gun-app.vercel.app/gun",
   "http://gun-manhattan.herokuapp.com/gun",
-]).get("gun-chat");
+]);
+
+const todoRef = gun.get("gun-chat");
 
 const Home: NextPage = () => {
   const [counter, setCounter] = useState(0);
-
+  const [currentTodo, setCurrentTodo] = useState("");
+  const [allTodos, setAllTodos] = useState<string[]>([]);
   useEffect(() => {
-    countRef.on((countVal) => {
+    todoRef.get("counter").on((countVal) => {
       setCounter(countVal.val);
-      console.log(countVal.val);
     });
+
+    // todoRef.get("todos").on(() => {
+    let newTodos: string[] = [];
+    console.group("Todos");
+    todoRef
+      .get("todos")
+      .map()
+      .map((todo) => {
+        newTodos.push(todo);
+      });
+    console.groupEnd();
+    setAllTodos(newTodos);
+    // });
+
+    todoRef.get("todos").on((todos) => {
+      console.log(todos);
+    });
+
+    // todoRef.get("todos").on((todos) => {
+    //   console.log("On todo", todos);
+    //   let newTodos: string[] = [];
+    //   todos.map((val:string) => newTodos.push(val));
+    //   setAllTodos(newTodos);
+    // });
   }, []);
 
   return (
@@ -23,7 +50,7 @@ const Home: NextPage = () => {
       {/* {count.get("val")} */}
       <button
         onClick={() => {
-          countRef.put({
+          todoRef.get("counter").put({
             val: counter - 1,
           });
           // setCounter(counter + 1);
@@ -34,7 +61,7 @@ const Home: NextPage = () => {
       {counter}
       <button
         onClick={() => {
-          countRef.put({
+          todoRef.get("counter").put({
             val: counter + 1,
           });
           // setCounter(counter + 1);
@@ -42,6 +69,82 @@ const Home: NextPage = () => {
       >
         +1
       </button>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter todo"
+          value={currentTodo}
+          onChange={(e) => {
+            setCurrentTodo(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            const todos = todoRef.get("todos");
+            todos.set({
+              val: currentTodo,
+            });
+
+            console.group("todos Add");
+            todoRef.get("todos").map((todo) => {
+              console.log(todo.val);
+            });
+            console.groupEnd();
+          }}
+        >
+          Add
+        </button>
+        <button
+          onClick={() => {
+            // console.log("clicked");
+            console.group("todos");
+            todoRef.get("todos").map((todo) => {
+              console.log(todo.val);
+            });
+            console.groupEnd();
+          }}
+        >
+          Get Todos
+        </button>
+        <button
+          onClick={() => {
+            // delete all todos
+            todoRef.get("todos").put({});
+            // print all todos
+            console.group("todos");
+            todoRef.get("todos").map((todo) => {
+              console.log(todo.val);
+            });
+            // todoRef.get("todos").map().unset();
+          }}
+        >
+          Remove all
+        </button>
+      </div>
+      <ul>
+        {allTodos.map((todo, index) => (
+          <div key={index} style={{ display: "flex" }}>
+            <li style={{ width: "5%" }}>{todo}</li>
+            <button
+              onClick={
+                // delete todo
+                () => {
+                  todoRef.get("todos").map((gun_todo) => {
+                    // console.log(todo.val);
+                    if (gun_todo.val === todo) {
+                      // console.log("deleting",gun_todo);
+                      console.log(Object.getOwnPropertyNames(gun_todo));
+                      // gun_todo.put(null);
+                    }
+                  });
+                }
+              }
+            >
+              {" X "}
+            </button>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 };
