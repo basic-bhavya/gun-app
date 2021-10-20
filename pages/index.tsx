1d0a027e6a3a870281e4e483e8744528d495fd6d
basic-bhavya <bjain.nd@gmail.com>
2021-10-20 22:49:41 +0530

    chat messages saving correctly

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,67 @@
 import type { NextPage } from "next";
 import Gun from "gun";
-// import "gun/lib/unset";
+require("gun/lib/unset");
 import { useEffect, useState } from "react";
+import { IGunChainReference } from "gun/types/chain";
 
 const gun = Gun([
   "localhost:3000/gun",
   "gun-app.vercel.app/gun",
-  "http://gun-manhattan.herokuapp.com/gun",
+  // "http://gun-manhattan.herokuapp.com/gun",
 ]);
+
+type TGunMessage = {
+  id: string;
+  sender: string;
+  message: string;
+};
 
 const chatRef = gun.get("gun-chat");
 
 const Home: NextPage = () => {
   const [counter, setCounter] = useState(0);
-  const [currentTodo, setCurrentTodo] = useState("");
-  const [allTodos, setAllTodos] = useState<string[]>([]);
+  const [chatMesssage, setChatMessage] = useState("");
+  const [allMessages, setAllMessages] = useState<TGunMessage | []>([]);
+  const sender = "PMalik";
+  // const [currentTodo, setCurrentTodo] = useState("");
+  // const [allTodos, setAllTodos] = useState<string[]>([]);
+
   useEffect(() => {
+    // chatRef.get("chats").put(null);
+
     chatRef.get("counter").on((countVal) => {
       setCounter(countVal.val);
     });
 
-    // todoRef.get("todos").on(() => {
-    let newTodos: string[] = [];
-    console.group("Todos");
-    chatRef
-      .get("todos")
-      .map()
-      .map((todo) => {
-        newTodos.push(todo);
-      });
-    console.groupEnd();
-    setAllTodos(newTodos);
-
-    chatRef.get("todos").on((todos) => {
-      console.log(todos);
+    chatRef.get("chat").map((chat: IGunChainReference) => {
+      console.log(chat);
+      // chatRef.get("chats").unset(chat);
+      // console.log(chat, typeof chat);
+      // console.log(chat.message, chat["_"]);
+      // const message: TGunMessage = {
+      //   id: chat["_"],
+      //   sender: chat.sender,
+      //   message: chat.message,
+      // };
+      // setAllMessages((allMessages) => [...allMessages]);
+      // console.log(chat.sender, ": ", chat.message);
     });
+
+    // todoRef.get("todos").on(() => {
+    // let newTodos: string[] = [];
+    // console.group("Todos");
+    // chatRef
+    //   .get("todos")
+    //   .map()
+    //   .map((todo) => {
+    //     newTodos.push(todo);
+    //   });
+    // console.groupEnd();
+    // setAllTodos(newTodos);
+
+    // chatRef.get("todos").on((todos) => {
+    //   console.log(todos);
+    // });
 
     // todoRef.get("todos").on((todos) => {
     //   console.log("On todo", todos);
@@ -70,6 +97,19 @@ const Home: NextPage = () => {
       </button>
       <div>
         <input
+          type="text"
+          placeholder="chat message"
+          onChange={(e) => setChatMessage(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            chatRef.get("chat").set({ sender: sender, message: chatMesssage });
+            setChatMessage("");
+          }}
+        >
+          Send
+        </button>
+        {/* <input
           type="text"
           placeholder="Enter todo"
           value={currentTodo}
@@ -118,10 +158,10 @@ const Home: NextPage = () => {
           }}
         >
           Remove all
-        </button>
+        </button> */}
       </div>
       <ul>
-        {allTodos.map((todo, index) => (
+        {/* {allTodos.map((todo, index) => (
           <div key={index} style={{ display: "flex" }}>
             <li style={{ width: "5%" }}>{todo}</li>
             <button
@@ -142,7 +182,7 @@ const Home: NextPage = () => {
               {" X "}
             </button>
           </div>
-        ))}
+        ))} */}
       </ul>
     </div>
   );
